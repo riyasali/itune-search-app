841347ef7902fe509fc61225c59902475836eb2d
Riyasali Kallayi <riyasalive@gmail.com>
2021-11-29 20:47:45 +0000

    feat: title name update

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ituneSearchApiUrl: 'http://localhost:3000/itunes/search'
 };
 
 /*
